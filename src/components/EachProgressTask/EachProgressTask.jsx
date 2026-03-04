9cca845d1a318352967712924e8d0d8a8f1fd094
pobittra chandra roy <devpobittra@gmail.com>
2026-03-04 12:43:04 +0600

    fix progress card responsive issue

--- src/components/EachProgressTask/EachProgressTask.jsx
+++ src/components/EachProgressTask/EachProgressTask.jsx
@@ -3,7 +3,7 @@ const EachProgressTask = ({ eachProgressTask, handleCompleteTask }) => {
     const { id, title } = eachProgressTask;
     return (
 
-        <div className="task-status-card bg-white text-black p-3 rounded-lg shadow-lg">
+        <div className="task-status-card bg-white text-black p-3 rounded-lg shadow-lg flex flex-col justify-between">
             <h3 className="text-xl font-bold capitalize">{title}</h3>
             <button className="py-1.5 bg-[#02A53B] text-white font-semibold cursor-pointer w-full text-center mt-2" onClick={() => handleCompleteTask(id)}>complete</button>
         </div>
