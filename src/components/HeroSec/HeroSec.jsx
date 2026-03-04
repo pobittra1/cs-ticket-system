f8d4aae20599fd2385aac52df4fe68284d8b76c9
pobittra chandra roy <devpobittra@gmail.com>
2026-03-04 09:55:52 +0600

    fix hero section issue

--- src/components/HeroSec/HeroSec.jsx
+++ src/components/HeroSec/HeroSec.jsx
@@ -3,7 +3,7 @@ import vector1 from "../../assets/vector1.png";
 const HeroSec = () => {
     return (
         <div className="p-4 min-h-70 flex flex-col md:flex-row justify-center items-center gap-6 md:gap-8">
-            <div className=" progress-card bg-linear-to-r from-purple-600 via-purple-500 to-purple-400 w-full md:w-1/2 h-45 rounded-xl flex items-center justify-between">
+            <div className=" progress-card bg-linear-to-r from-purple-600 via-purple-500 to-purple-400 w-full md:w-1/2 h-45 rounded-xl grid grid-cols-3">
                 <img className="h-full object-contain" src={vector1} alt="vector image" />
                 <div className="flex flex-col justify-center items-center font-bold">
                     <h2>In-Progress</h2>
@@ -11,7 +11,7 @@ const HeroSec = () => {
                 </div>
                 <img className="h-full object-contain -scale-x-100" src={vector1} alt="vector image" />
             </div>
-            <div className="progress-card bg-linear-to-r from-green-600 via-green-500 to-green-400 w-full md:w-1/2 h-45 rounded-xl flex items-center justify-between">
+            <div className="progress-card bg-linear-to-r from-green-600 via-green-500 to-green-400 w-full md:w-1/2 h-45 rounded-xl grid grid-cols-3">
                 <img className="h-full object-contain" src={vector1} alt="vector image" />
                 <div className="flex flex-col justify-center items-center font-bold">
                     <h2>Resolved</h2>
